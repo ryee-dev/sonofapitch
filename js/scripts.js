eb64c6564d584031442ca3ab859e7adbade429ac
johnny mayer <johnnymayer@gmail.com>
2018-01-30 14:32:10 -0800

    Add files via upload
    Upload of working files.

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,11 @@
-function Scale(root){
-  this.pianoKeyArray = ["C4", "C#4", "D4", "D#4", "E4", "F4", "F#4", "G4", "G#4", "A4", "A#4", "B4", "C5", "C#5", "D5", "D#5", "E5", "F5", "F#5", "G5", "G#5", "A5", "A#5", "B5", "C6", "C#6", "D6", "D#6", "E6", "F6", "F#6", "G6", "G#6", "A6", "A#6", "B6" ]
+function Scale(root) {
+  this.pianoKeyArray = ["C4", "C#4", "D4", "D#4", "E4", "F4", "F#4", "G4", "G#4", "A4", "A#4", "B4", "C5", "C#5", "D5", "D#5", "E5", "F5", "F#5", "G5", "G#5", "A5", "A#5", "B5", "C6", "C#6", "D6", "D#6", "E6", "F6", "F#6", "G6", "G#6", "A6", "A#6", "B6"]
   this.root = root;
   this.rootIndex = this.pianoKeyArray.indexOf(this.root);
   this.scaleArray = [];
 }
 
-Scale.prototype.createMajorScaleArray = function(){
+Scale.prototype.createMajorScaleArray = function() {
   this.scaleArray.push(this.pianoKeyArray[this.rootIndex]);
   this.scaleArray.push(this.pianoKeyArray[this.rootIndex + 2]);
   this.scaleArray.push(this.pianoKeyArray[this.rootIndex + 4]);
@@ -14,11 +14,10 @@ Scale.prototype.createMajorScaleArray = function(){
   this.scaleArray.push(this.pianoKeyArray[this.rootIndex + 9]);
   this.scaleArray.push(this.pianoKeyArray[this.rootIndex + 11]);
   this.scaleArray.push(this.pianoKeyArray[this.rootIndex + 12]);
-
   return this.scaleArray;
 }
 
-Scale.prototype.createMinorScaleArray = function(){
+Scale.prototype.createMinorScaleArray = function() {
   this.scaleArray.push(this.pianoKeyArray[this.rootIndex]);
   this.scaleArray.push(this.pianoKeyArray[this.rootIndex + 2]);
   this.scaleArray.push(this.pianoKeyArray[this.rootIndex + 3]);
@@ -29,16 +28,21 @@ Scale.prototype.createMinorScaleArray = function(){
   return this.scaleArray;
 }
 
-
-
 $(document).ready(function() {
-  $(".key").click(function(){
+  $(".key").click(function() {
+    $("#scaleList").empty();
     var keyPick = ($(this).text());
-    alert($(this).text());
     var newScale = new Scale(keyPick);
-    var majorScaleArray = newScale.createMajorScaleArray();
-    alert(majorScaleArray);
+    if ($("#major").is(":checked")){
+      $("#scaleList").empty();
+      var majorScaleArray = newScale.createMajorScaleArray();
+      $("#scaleList").append("<p>" + majorScaleArray + "</p>");
+    } else if ($("#minor").is(":checked")){
+      $("#scaleList").empty();
+      var minorScaleArray = newScale.createMinorScaleArray();
+      $("#scaleList").append("<p>" + minorScaleArray + "</p>");
+    } else {
+      alert("Pick Major, Minor, or Pentatonic.");
+    }
   });
-
-  $(".key")
 });
