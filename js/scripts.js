df23af0a1feabab26e97c66b49bb4b79dbcc71c4
roger yee <ryee.epicodus@gmail.com>
2018-01-29 19:13:49 -0800

    ended up downloading the zip because I couldn't figure out how to properly clone.

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,40 @@
 $(document).ready(function() {
     $("#c4").click(function(){
-      $("#results").fadeToggle();
-      $(".c").fadeToggle();
+      $("#results").fadeIn();
+      $(".c").fadeIn();
+
     });
-    $(".keys").click(function(){
-      ;
+    $("#d4").click(function(){
+      $("#results").fadeIn();
+      $(".d").fadeIn();
+
+    })
+    $("#e4").click(function(){
+      $("#results").fadeIn();
+      $(".e").fadeIn();
+    })
+    $("#f4").click(function(){
+      $("#results").fadeIn();
+      $(".f").fadeIn();
+    })
+    $("#kb-g4").click(function(){
+      $("#results").fadeIn();
+      $(".g").fadeIn();
+    })
+    $("#kb-a5").click(function(){
+      $("#results").fadeIn();
+      $(".a").fadeIn();
+    })
+    $("#kb-b5").click(function(){
+      $("#results").fadeIn();
+      $(".b").fadeIn();
+    })
+    $("#kb-c5").click(function(){
+      $("#results").fadeIn();
+      $(".c").fadeIn();
+    })
+
+    $("#close-button").click(function(){
+      $("#results").fadeOut();
     })
 });
