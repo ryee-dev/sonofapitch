9d75f00293cb3046026ec2b5c4dc7c78b2384d00
johnny mayer <johnnymayer@gmail.com>
2018-01-30 10:12:16 -0800

    Add files via upload
    New testing branch, implement click function for individual keys.

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,7 @@
+
+
 $(document).ready(function() {
-    $("#c4").click(function(){
-      $("#results").fadeToggle();
-      $(".c").fadeToggle();
-    });
-    $(".keys").click(function(){
-      ;
-    })
+  $(".ivory").click(function(){
+    alert($(this).text());
+  });
 });
