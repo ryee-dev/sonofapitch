e28b24f8bc1271773bd8b92eb08b5ce9ad517ac9
KaitlynFujihara <30393329+KaitlynFujihara@users.noreply.github.com>
2018-01-31 14:55:58 -0800

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -5,22 +5,74 @@ function Scale(root) {
   this.scaleArray = [];
 }
 
-function Info(root, scaleArray, outputArray, scaleImage) {
-  this.root = root;
-  this.scaleArray = scaleArray;
-  this.outputArray = outputArray;
-  this.scaleImage = scaleImage;
+Scale.prototype.createSongOne = function() {
+  this.scaleArray.push(this.pianoKeyArray[this.rootIndex]);
+  this.scaleArray.push(this.pianoKeyArray[this.rootIndex + 4]);
+  this.scaleArray.push(this.pianoKeyArray[this.rootIndex + 2]);
+  this.scaleArray.push(this.pianoKeyArray[this.rootIndex]);
+  this.scaleArray.push(this.pianoKeyArray[this.rootIndex + 2]);
+  this.scaleArray.push(this.pianoKeyArray[this.rootIndex + 4]);
+  this.scaleArray.push(this.pianoKeyArray[this.rootIndex + 4]);
+  this.scaleArray.push(this.pianoKeyArray[this.rootIndex + 4]);
+  this.scaleArray.push(this.pianoKeyArray[this.rootIndex + 2]);
+  this.scaleArray.push(this.pianoKeyArray[this.rootIndex + 2]);
+  this.scaleArray.push(this.pianoKeyArray[this.rootIndex + 2]);
+  this.scaleArray.push(this.pianoKeyArray[this.rootIndex + 4]);
+  this.scaleArray.push(this.pianoKeyArray[this.rootIndex + 4]);
+  this.scaleArray.push(this.pianoKeyArray[this.rootIndex + 4]);
+  this.scaleArray.push(this.pianoKeyArray[this.rootIndex + 4]);
+  this.scaleArray.push(this.pianoKeyArray[this.rootIndex + 2]);
+  this.scaleArray.push(this.pianoKeyArray[this.rootIndex]);
+  this.scaleArray.push(this.pianoKeyArray[this.rootIndex + 2]);
+  this.scaleArray.push(this.pianoKeyArray[this.rootIndex + 4]);
+  this.scaleArray.push(this.pianoKeyArray[this.rootIndex + 4]);
+  this.scaleArray.push(this.pianoKeyArray[this.rootIndex + 4]);
+  this.scaleArray.push(this.pianoKeyArray[this.rootIndex + 4]);
+  this.scaleArray.push(this.pianoKeyArray[this.rootIndex + 2]);
+  this.scaleArray.push(this.pianoKeyArray[this.rootIndex + 2]);
+  this.scaleArray.push(this.pianoKeyArray[this.rootIndex + 4]);
+  this.scaleArray.push(this.pianoKeyArray[this.rootIndex + 2]);
+  this.scaleArray.push(this.pianoKeyArray[this.rootIndex]);
 }
 
-Info.prototype.scaleType = function() {
-  this.outputArray.concat(this.scaleArray);
-  return this.outputArray;
+Scale.prototype.createSongTwo = function() {
+  this.scaleArray.push(this.pianoKeyArray[this.rootIndex]);
+  this.scaleArray.push(this.pianoKeyArray[this.rootIndex]);
+  this.scaleArray.push(this.pianoKeyArray[this.rootIndex]);
+  this.scaleArray.push(this.pianoKeyArray[this.rootIndex]);
+  this.scaleArray.push(this.pianoKeyArray[this.rootIndex + 5]);
+  this.scaleArray.push(this.pianoKeyArray[this.rootIndex + 5]);
+  this.scaleArray.push(this.pianoKeyArray[this.rootIndex + 5]);
+  this.scaleArray.push(this.pianoKeyArray[this.rootIndex + 5]);
+  this.scaleArray.push(this.pianoKeyArray[this.rootIndex + 7]);
+  this.scaleArray.push(this.pianoKeyArray[this.rootIndex + 7]);
+  this.scaleArray.push(this.pianoKeyArray[this.rootIndex + 7]);
+  this.scaleArray.push(this.pianoKeyArray[this.rootIndex + 7]);
+  this.scaleArray.push(this.pianoKeyArray[this.rootIndex + 5]);
+  this.scaleArray.push(this.pianoKeyArray[this.rootIndex + 5]);
+  this.scaleArray.push(this.pianoKeyArray[this.rootIndex + 5]);
+  this.scaleArray.push(this.pianoKeyArray[this.rootIndex + 5]);
 }
 
-Info.prototype.addImage = function() {
-  this.scaleImage = "<img src=" + "img/" + keyPick + ".png>";
-  return this.scaleImage;
+Scale.prototype.createSongThree = function() {
+  this.scaleArray.push(this.pianoKeyArray[this.rootIndex]);
+  this.scaleArray.push(this.pianoKeyArray[this.rootIndex]);
+  this.scaleArray.push(this.pianoKeyArray[this.rootIndex]);
+  this.scaleArray.push(this.pianoKeyArray[this.rootIndex]);
+  this.scaleArray.push(this.pianoKeyArray[this.rootIndex + 1]);
+  this.scaleArray.push(this.pianoKeyArray[this.rootIndex + 1]);
+  this.scaleArray.push(this.pianoKeyArray[this.rootIndex + 1]);
+  this.scaleArray.push(this.pianoKeyArray[this.rootIndex + 1]);
+  this.scaleArray.push(this.pianoKeyArray[this.rootIndex + 3]);
+  this.scaleArray.push(this.pianoKeyArray[this.rootIndex + 3]);
+  this.scaleArray.push(this.pianoKeyArray[this.rootIndex + 3]);
+  this.scaleArray.push(this.pianoKeyArray[this.rootIndex + 3]);
+  this.scaleArray.push(this.pianoKeyArray[this.rootIndex + 1]);
+  this.scaleArray.push(this.pianoKeyArray[this.rootIndex + 1]);
+  this.scaleArray.push(this.pianoKeyArray[this.rootIndex + 1]);
+  this.scaleArray.push(this.pianoKeyArray[this.rootIndex + 1]);
 }
+
 
 Scale.prototype.createMajorScaleArray = function() {
   this.scaleArray.push(this.pianoKeyArray[this.rootIndex]);
@@ -49,65 +101,210 @@ Scale.prototype.createMinorScaleArray = function() {
 function playNote(note){
   var synth = new Tone.Synth().toMaster()
   synth.triggerAttackRelease(note, '8n')
+  // /alert(note.replace("#", "sharp"));
 }
 
 Scale.prototype.playScale = function(){
   var delayInMilliseconds = 0;
   this.scaleArray.forEach(function(note){
-
     setTimeout(function() {
       playNote(note);
+      $("#"+(note.replace('#','s'))).toggleClass("highlight");
     }, delayInMilliseconds);
-
+    setTimeout(function() {
+      $("#"+(note.replace('#','s'))).toggleClass("highlight");
+    }, delayInMilliseconds + 350);
     delayInMilliseconds += 300;
-
+    setTimeout(function() {
+      $(".key").css("pointer-events", "auto");
+    }, 2400);
   });
 }
 
 $(document).ready(function() {
+  $(document).keydown(function(keyPressed) {
+    if (keyPressed.keyCode == 65) {
+      $("#C4").toggleClass("highlight");
+      setTimeout(function () {
+       $("#C4").removeClass("highlight");
+      },200);
+    playNote("C4");
+    } else if (keyPressed.keyCode == 68) {
+      $("#E4").toggleClass("highlight");
+      setTimeout(function () {
+       $("#E4").removeClass("highlight");
+     },200);
+    playNote("E4");
+    }  else if (keyPressed.keyCode == 83) {
+      $("#D4").toggleClass("highlight");
+      setTimeout(function () {
+       $("#D4").removeClass("highlight");
+     },200);
+    playNote("D4");
+    } else if (keyPressed.keyCode == 70) {
+      $("#F4").toggleClass("highlight");
+      setTimeout(function () {
+       $("#F4").removeClass("highlight");
+     },200);
+    playNote("F4");
+    } else if (keyPressed.keyCode == 71) {
+      $("#G4").toggleClass("highlight");
+      setTimeout(function () {
+       $("#G4").removeClass("highlight");
+     },200);
+    playNote("G4");
+    } else if (keyPressed.keyCode == 72) {
+      $("#A4").toggleClass("highlight");
+      setTimeout(function () {
+       $("#A4").removeClass("highlight");
+     },200);
+    playNote("A4");
+    } else if (keyPressed.keyCode == 74) {
+      $("#B4").toggleClass("highlight");
+      setTimeout(function () {
+       $("#B4").removeClass("highlight");
+     },200);
+    playNote("B4");
+    } else if (keyPressed.keyCode == 75) {
+      $("#C5").toggleClass("highlight");
+      setTimeout(function () {
+       $("#C5").removeClass("highlight");
+     },200);
+    playNote("C5");
+    } else if (keyPressed.keyCode == 76) {
+      $("#D5").toggleClass("highlight");
+      setTimeout(function () {
+       $("#D5").removeClass("highlight");
+     },200);
+    playNote("D5");
+    } else if (keyPressed.keyCode == 186) {
+      $("#E5").toggleClass("highlight");
+      setTimeout(function () {
+       $("#E5").removeClass("highlight");
+     },200);
+    playNote("E5");
+    } else if (keyPressed.keyCode == 222) {
+      $("#F5").toggleClass("highlight");
+      setTimeout(function () {
+       $("#F5").removeClass("highlight");
+     },200);
+    playNote("F5");
+    } else if (keyPressed.keyCode == 13) {
+      $("#G5").toggleClass("highlight");
+      setTimeout(function () {
+       $("#G5").removeClass("highlight");
+     },200);
+    playNote("G5");
+    } else if (keyPressed.keyCode == 191) {
+      $("#A5").toggleClass("highlight");
+      setTimeout(function () {
+       $("#A5").removeClass("highlight");
+     },200);
+    playNote("A5");
+    } else if (keyPressed.keyCode == 16) {
+      $("#B5").toggleClass("highlight");
+      setTimeout(function () {
+       $("#B5").removeClass("highlight");
+     },200);
+    playNote("B5");
+    } else if (keyPressed.keyCode == 87) {
+      $("#Cs4").toggleClass("highlight");
+      setTimeout(function () {
+       $("#Cs4").removeClass("highlight");
+     },200);
+    playNote("C#4");
+    }  else if (keyPressed.keyCode == 69) {
+      $("#Ds4").toggleClass("highlight");
+      setTimeout(function () {
+       $("#Ds4").removeClass("highlight");
+     },200);
+    playNote("D#4");
+    } else if (keyPressed.keyCode == 82) {
+      $("#Fs4").toggleClass("highlight");
+      setTimeout(function () {
+       $("#Fs4").removeClass("highlight");
+     },200);
+    playNote("F#4");
+    } else if (keyPressed.keyCode == 84) {
+      $("#Gs4").toggleClass("highlight");
+      setTimeout(function () {
+       $("#Gs4").removeClass("highlight");
+     },200);
+    playNote("G#4");
+    } else if (keyPressed.keyCode == 89) {
+      $("#As4").toggleClass("highlight");
+      setTimeout(function () {
+       $("#As4").removeClass("highlight");
+     },200);
+    playNote("A#4");
+    } else if (keyPressed.keyCode == 85) {
+      $("#Cs5").toggleClass("highlight");
+      setTimeout(function () {
+       $("#Cs5").removeClass("highlight");
+     },200);
+    playNote("C#5");
+    } else if (keyPressed.keyCode ==73) {
+      $("#Ds5").toggleClass("highlight");
+      setTimeout(function () {
+       $("#Ds5").removeClass("highlight");
+     },200);
+    playNote("D#5");
+    } else if (keyPressed.keyCode == 79) {
+      $("#Fs5").toggleClass("highlight");
+      setTimeout(function () {
+       $("#Fs5").removeClass("highlight");
+     },200);
+    playNote("F#5");
+    } else if (keyPressed.keyCode == 80) {
+      $("#Gs5").toggleClass("highlight");
+      setTimeout(function () {
+       $("#Gs5").removeClass("highlight");
+     },200);
+    playNote("G#5");
+    } else if (keyPressed.keyCode == 219) {
+      $("#As5").toggleClass("highlight");
+      setTimeout(function () {
+       $("#As5").removeClass("highlight");
+     },200);
+    playNote("A#5");
+    }
+    });
 
-  var majorScaleArray = [];
-  var minorScaleArray = [];
-  var keyPick = "";
-  var newScale = [];
-  var newInfo = [];
-  var sliderVal = "";
 
     $(".key").click(function() {
       $(".key").css("pointer-events", "none");
       $("#scaleList").empty();
-      keyPick = ($(this).text());
-      newScale = new Scale(keyPick);
-      newInfo = new Info(keyPick);
-      sliderVal = $("#major-minor").val();
+      var keyPick = ($(this).text());
+      var newScale = new Scale(keyPick);
+      var sliderVal = $("#major-minor").val();
 
       if (sliderVal === "1") {
-        $("#scaleList").empty();
-        majorScaleArray = newScale.createMajorScaleArray();
-        newScale.playScale();
-        $("#scaleList").append("<p>" + majorScaleArray + "</p>");
-
-        setTimeout(function() {
-          $(".key").css("pointer-events", "auto");
-        }, 2400);
-
+          $("#scaleList").empty();
+          var majorScaleArray = newScale.createMajorScaleArray();
+          newScale.playScale();
+          $("#scaleList").append("<p>" + majorScaleArray + "</p>");
       } else if (sliderVal === "3") {
-        $("#scaleList").empty();
-        minorScaleArray = newScale.createMinorScaleArray();
-        newScale.playScale();
-        $("#results").show();
-        $("#scaleList").append("<p>" + minorScaleArray + "</p>");
-
-        setTimeout(function() {
-          $(".key").css("pointer-events", "auto");
-        }, 2400);
-
+          $("#scaleList").empty();
+          var minorScaleArray = newScale.createMinorScaleArray();
+          newScale.playScale();
+          $("#scaleList").append("<p>" + minorScaleArray + "</p>");
+      } else if (sliderVal === "4") {
+          $("#scaleList").empty();
+          var createSongOne = newScale.createSongOne();
+          newScale.playScale();
+      } else if (sliderVal === "5") {
+          $("#scaleList").empty();
+          var createSongTwo = newScale.createSongTwo();
+          newScale.playScale();
+      } else if (sliderVal === "6") {
+          $("#scaleList").empty();
+          var createSongThree = newScale.createSongThree();
+          newScale.playScale();
       } else {
-        playNote(keyPick);
-        $(".key").css("pointer-events", "auto");
+          playNote(keyPick);
+          $(".key").css("pointer-events", "auto");
       }
 
-      console.log(sliderVal);
 
     });
 
