0fbfe34f925afdf38592ade414ef8ade7fb366ab
Guest User <Guest@Epicodus-7B.local>
2018-01-31 15:33:22 -0800

    Merged all files for final MVP

--- js/scripts.js
+++ js/scripts.js
@@ -98,17 +98,16 @@ Scale.prototype.createMinorScaleArray = function() {
   return this.scaleArray;
 }
 
-function playNote(note){
-  var synth = new Tone.Synth().toMaster()
+function playNote(note, synth){
   synth.triggerAttackRelease(note, '8n')
   // /alert(note.replace("#", "sharp"));
 }
 
-Scale.prototype.playScale = function(){
+Scale.prototype.playScale = function(synth){
   var delayInMilliseconds = 0;
   this.scaleArray.forEach(function(note){
     setTimeout(function() {
-      playNote(note);
+      playNote(note, synth);
       $("#"+(note.replace('#','s'))).toggleClass("highlight");
     }, delayInMilliseconds);
     setTimeout(function() {
@@ -122,151 +121,152 @@ Scale.prototype.playScale = function(){
 }
 
 $(document).ready(function() {
+  var theSynth = new Tone.Synth().toMaster();
   $(document).keydown(function(keyPressed) {
     if (keyPressed.keyCode == 65) {
       $("#C4").toggleClass("highlight");
       setTimeout(function () {
        $("#C4").removeClass("highlight");
       },200);
-    playNote("C4");
+    playNote("C4", theSynth);
     } else if (keyPressed.keyCode == 68) {
       $("#E4").toggleClass("highlight");
       setTimeout(function () {
        $("#E4").removeClass("highlight");
      },200);
-    playNote("E4");
+    playNote("E4", theSynth);
     }  else if (keyPressed.keyCode == 83) {
       $("#D4").toggleClass("highlight");
       setTimeout(function () {
        $("#D4").removeClass("highlight");
      },200);
-    playNote("D4");
+    playNote("D4", theSynth);
     } else if (keyPressed.keyCode == 70) {
       $("#F4").toggleClass("highlight");
       setTimeout(function () {
        $("#F4").removeClass("highlight");
      },200);
-    playNote("F4");
+    playNote("F4", theSynth);
     } else if (keyPressed.keyCode == 71) {
       $("#G4").toggleClass("highlight");
       setTimeout(function () {
        $("#G4").removeClass("highlight");
      },200);
-    playNote("G4");
+    playNote("G4", theSynth);
     } else if (keyPressed.keyCode == 72) {
       $("#A4").toggleClass("highlight");
       setTimeout(function () {
        $("#A4").removeClass("highlight");
      },200);
-    playNote("A4");
+    playNote("A4", theSynth);
     } else if (keyPressed.keyCode == 74) {
       $("#B4").toggleClass("highlight");
       setTimeout(function () {
        $("#B4").removeClass("highlight");
      },200);
-    playNote("B4");
+    playNote("B4", theSynth);
     } else if (keyPressed.keyCode == 75) {
       $("#C5").toggleClass("highlight");
       setTimeout(function () {
        $("#C5").removeClass("highlight");
      },200);
-    playNote("C5");
+    playNote("C5", theSynth);
     } else if (keyPressed.keyCode == 76) {
       $("#D5").toggleClass("highlight");
       setTimeout(function () {
        $("#D5").removeClass("highlight");
      },200);
-    playNote("D5");
+    playNote("D5", theSynth);
     } else if (keyPressed.keyCode == 186) {
       $("#E5").toggleClass("highlight");
       setTimeout(function () {
        $("#E5").removeClass("highlight");
      },200);
-    playNote("E5");
+    playNote("E5", theSynth);
     } else if (keyPressed.keyCode == 222) {
       $("#F5").toggleClass("highlight");
       setTimeout(function () {
        $("#F5").removeClass("highlight");
      },200);
-    playNote("F5");
+    playNote("F5", theSynth);
     } else if (keyPressed.keyCode == 13) {
       $("#G5").toggleClass("highlight");
       setTimeout(function () {
        $("#G5").removeClass("highlight");
      },200);
-    playNote("G5");
+    playNote("G5", theSynth);
     } else if (keyPressed.keyCode == 191) {
       $("#A5").toggleClass("highlight");
       setTimeout(function () {
        $("#A5").removeClass("highlight");
      },200);
-    playNote("A5");
+    playNote("A5", theSynth);
     } else if (keyPressed.keyCode == 16) {
       $("#B5").toggleClass("highlight");
       setTimeout(function () {
        $("#B5").removeClass("highlight");
      },200);
-    playNote("B5");
+    playNote("B5", theSynth);
     } else if (keyPressed.keyCode == 87) {
       $("#Cs4").toggleClass("highlight");
       setTimeout(function () {
        $("#Cs4").removeClass("highlight");
      },200);
-    playNote("C#4");
+    playNote("C#4", theSynth);
     }  else if (keyPressed.keyCode == 69) {
       $("#Ds4").toggleClass("highlight");
       setTimeout(function () {
        $("#Ds4").removeClass("highlight");
      },200);
-    playNote("D#4");
+    playNote("D#4", theSynth);
     } else if (keyPressed.keyCode == 82) {
       $("#Fs4").toggleClass("highlight");
       setTimeout(function () {
        $("#Fs4").removeClass("highlight");
      },200);
-    playNote("F#4");
+    playNote("F#4", theSynth);
     } else if (keyPressed.keyCode == 84) {
       $("#Gs4").toggleClass("highlight");
       setTimeout(function () {
        $("#Gs4").removeClass("highlight");
      },200);
-    playNote("G#4");
+    playNote("G#4", theSynth);
     } else if (keyPressed.keyCode == 89) {
       $("#As4").toggleClass("highlight");
       setTimeout(function () {
        $("#As4").removeClass("highlight");
      },200);
-    playNote("A#4");
+    playNote("A#4", theSynth);
     } else if (keyPressed.keyCode == 85) {
       $("#Cs5").toggleClass("highlight");
       setTimeout(function () {
        $("#Cs5").removeClass("highlight");
      },200);
-    playNote("C#5");
+    playNote("C#5", theSynth);
     } else if (keyPressed.keyCode ==73) {
       $("#Ds5").toggleClass("highlight");
       setTimeout(function () {
        $("#Ds5").removeClass("highlight");
      },200);
-    playNote("D#5");
+    playNote("D#5", theSynth);
     } else if (keyPressed.keyCode == 79) {
       $("#Fs5").toggleClass("highlight");
       setTimeout(function () {
        $("#Fs5").removeClass("highlight");
      },200);
-    playNote("F#5");
+    playNote("F#5", theSynth);
     } else if (keyPressed.keyCode == 80) {
       $("#Gs5").toggleClass("highlight");
       setTimeout(function () {
        $("#Gs5").removeClass("highlight");
      },200);
-    playNote("G#5");
+    playNote("G#5", theSynth);
     } else if (keyPressed.keyCode == 219) {
       $("#As5").toggleClass("highlight");
       setTimeout(function () {
        $("#As5").removeClass("highlight");
      },200);
-    playNote("A#5");
+    playNote("A#5", theSynth);
     }
     });
 
@@ -278,30 +278,30 @@ $(document).ready(function() {
       var newScale = new Scale(keyPick);
       var sliderVal = $("#major-minor").val();
 
-      if (sliderVal === "1") {
+      if (sliderVal === "2") {
           $("#scaleList").empty();
           var majorScaleArray = newScale.createMajorScaleArray();
-          newScale.playScale();
+          newScale.playScale(theSynth);
           $("#scaleList").append("<p>" + majorScaleArray + "</p>");
       } else if (sliderVal === "3") {
           $("#scaleList").empty();
           var minorScaleArray = newScale.createMinorScaleArray();
-          newScale.playScale();
+          newScale.playScale(theSynth);
           $("#scaleList").append("<p>" + minorScaleArray + "</p>");
       } else if (sliderVal === "4") {
           $("#scaleList").empty();
           var createSongOne = newScale.createSongOne();
-          newScale.playScale();
+          newScale.playScale(theSynth);
       } else if (sliderVal === "5") {
           $("#scaleList").empty();
           var createSongTwo = newScale.createSongTwo();
-          newScale.playScale();
+          newScale.playScale(theSynth);
       } else if (sliderVal === "6") {
           $("#scaleList").empty();
           var createSongThree = newScale.createSongThree();
-          newScale.playScale();
+          newScale.playScale(theSynth);
       } else {
-          playNote(keyPick);
+          playNote(keyPick, theSynth);
           $(".key").css("pointer-events", "auto");
       }
 
